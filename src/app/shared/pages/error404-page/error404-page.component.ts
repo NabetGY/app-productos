4b4c66d32344710a39e1376ddac0c1138b6d6f54
nabet <yoan.lopez@utp.edu.co>
2024-10-09 16:52:32 -0500

    fix: Login/Register text

--- src/app/shared/pages/error404-page/error404-page.component.ts
+++ src/app/shared/pages/error404-page/error404-page.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-error404-page',
   templateUrl: './error404-page.component.html',
-  styles: ``
+  styles: `
+  .auth-background {
+  background: rgb(2,0,36);
+  background: linear-gradient(90deg, #4601bd 0%, #673AB7 35%, rgba(0,212,255,1) 100%);
+}
+
+  `
 })
 export class Error404PageComponent {
 
